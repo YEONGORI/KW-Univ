9751e09c63c7a884066aed558212911679ebd656
YEONGORI <dusrjf14@gmail.com>
2022-11-04 02:42:15 -0700

    3-2 doing

---
+++ 3학년 2학기/운영체제/as2/assignment2/filegen.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <dirent.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+
+#define MAX_PROCESS 100
+
+int main(void) {
+    char num[100];
+    mkdir("temp", 0777);
+    DIR *dir = opendir("temp");
+    for (int i=0; i<MAX_PROCESS; i++) {
+        char file_name[10000] = {'t','e','m','p','/','\0'};
+        sprintf(num, "%d", i);
+        // file_name = strcat(file_name, num);
+        FILE *f_write = fopen(strcat(file_name, num), "w+");
+        fprintf(f_write, "%d", 1+rand()%9);
+        fclose(f_write);
+    }
+    closedir(dir);
+}
