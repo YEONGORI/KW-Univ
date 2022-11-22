6deb50cce295b178b06c04a5aa1bb24d112e14c7
YEONGORI <dusrjf14@gmail.com>
2022-11-22 02:44:48 -0800

    fix: seg fault

--- as3/4-2/D_recompile.c
+++ as3/4-2/D_recompile.c
@@ -1,3 +1,4 @@
+#include <time.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <stdint.h> 
@@ -23,7 +24,9 @@ void* drecompile(uint8_t *func); //����ȭ�ϴ� �κ�
 int main(void)
 {
 	int (*func)(int a);
-	int i;
+	clock_t start, end;
+
+	start = clock();
 
 	sharedmem_init();
 	drecompile_init();
@@ -32,14 +35,18 @@ int main(void)
 
 	drecompile_exit();
 	sharedmem_exit();
+
+	end = clock();
+	printf("Total execution time %lf sec\n", (double)(end-start)/CLOCKS_PER_SEC);
+
 	return 0;
 }
 
 void sharedmem_init()
 {
-	segment_id = shmget(1234, PAGE_SIZE, IPC_CREAT | S_IRUSR | S_IWUSR);
+	page_size = getpagesize();
+	segment_id = shmget(1234, page_size, IPC_CREAT | S_IRUSR | S_IWUSR);
 	Operation = (uint8_t*)shmat(segment_id, NULL, 0);
-
 }
 
 void sharedmem_exit()
@@ -50,25 +57,23 @@ void sharedmem_exit()
 
 void drecompile_init()
 {
-	page_size = getpagesize();
-
 	compiled_code = mmap(NULL, page_size, PROT_READ | PROT_WRITE, MAP_SHARED, -1, 0);
-	memcpy(compiled_code, Operation, page_size);
+	compiled_code = Operation; // ....
 }
 
 void drecompile_exit()
 {
+	msync(compiled_code, page_size, MS_ASYNC);
 	munmap(compiled_code, page_size);
 }
 
 void* drecompile(uint8_t* func)
 {
 	int i = 0, j = 0;
-	uint8_t tmp;
 
-#ifdef DYNAMIC
-	while (func[i] != 0xc3) {
-		tmp = 0;
+#ifdef dynamic
+	while (func[i] != 0xc3 && i < page_size && j < page_size) {
+		uint8_t tmp = 0;
 		if (func[i] == 0x83 && func[i + 1] == 0xc0) // add
 		{
 			for (; func[i] == 0x83 && func[i + 1] == 0xc0; i += 3)
@@ -76,12 +81,6 @@ void* drecompile(uint8_t* func)
 			compiled_code[j++] = func[i - 3];
 			compiled_code[j++] = func[i - 2];
 			compiled_code[j++] = tmp;
-			// while (func[i] == 0x83 && func[i + 1] == 0xc0)
-			// {
-			// 	tmp += func[i + 2];
-			// 	i += 3;
-			// }
-
 		}
 		else if (func[i] == 0x83 && func[i + 1] == 0xe8) // sub
 		{
@@ -90,11 +89,6 @@ void* drecompile(uint8_t* func)
 			compiled_code[j++] = func[i - 3];
 			compiled_code[j++] = func[i - 2];
 			compiled_code[j++] = tmp;
-			// while (func[i] == 0x83 && func[i + 1] == 0xc0)
-			// {
-			// 	tmp -= func[i + 2];
-			// 	i += 3;
-			// }
 		}
 		else if (func[i] == 0x6b) // imul
 		{
@@ -104,24 +98,16 @@ void* drecompile(uint8_t* func)
 			compiled_code[j++] = func[i - 3];
 			compiled_code[j++] = func[i - 2];
 			compiled_code[j++] = tmp;
-			// while (func[i] == 0x6b)
-			// {
-			// 	tmp
-			// }
 		}
 		else if (func[i] == 0xf6) // div
 		{
 			i += 2;
-			// while (func[i] == 0xf6)
-			// {
-			// 	tmp *= func[i + 2];
-			// 	i += 3;
-			// }
 		}
-		else
+		else // nothing
 			compiled_code[j++] = func[i++];
 	}
-	compiled_code[j] = func[i];
+	printf("DYNAMIC!\n");
+	compiled_code[j - 1] = 0xc3;
 #endif
 	mprotect(compiled_code, page_size, PROT_READ | PROT_EXEC);
 	return compiled_code;
