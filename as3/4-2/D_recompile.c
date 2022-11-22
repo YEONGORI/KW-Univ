807770ce743865fa639c64c44d1b84e986c3c866
YEONGORI <dusrjf14@gmail.com>
2022-11-22 05:56:25 -0800

    fix: floating point exception

--- as3/4-2/D_recompile.c
+++ as3/4-2/D_recompile.c
@@ -1,5 +1,7 @@
 #include <time.h>
+#include <fcntl.h>
 #include <stdio.h>
+#include <stdint.h>
 #include <unistd.h>
 #include <stdint.h> 
 #include <string.h>
@@ -15,11 +17,11 @@ int	segment_id, page_size;
 uint8_t* Operation;
 uint8_t* compiled_code;
 
-void sharedmem_init(); // ���� �޸𸮿� ����
+void sharedmem_init();
 void sharedmem_exit();
-void drecompile_init(); // memory mapping ���� 
-void drecompile_exit(); 
-void* drecompile(uint8_t *func); //����ȭ�ϴ� �κ�
+void drecompile_init();
+void drecompile_exit();
+void* drecompile(uint8_t *func);
 
 int main(void)
 {
@@ -32,6 +34,8 @@ int main(void)
 	drecompile_init();
 
 	func = (int (*)(int a))drecompile(Operation);
+
+	printf("func: %d\n", func(1));
 
 	drecompile_exit();
 	sharedmem_exit();
@@ -57,59 +61,112 @@ void sharedmem_exit()
 
 void drecompile_init()
 {
-	compiled_code = mmap(NULL, page_size, PROT_READ | PROT_WRITE, MAP_SHARED, -1, 0);
-	compiled_code = Operation; // ....
+	int fd = open("text", O_RDWR|O_CREAT, S_IRUSR|S_IWUSR);
+	compiled_code = mmap(NULL, page_size * 2, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+	compiled_code = Operation;
 }
 
 void drecompile_exit()
 {
-	msync(compiled_code, page_size, MS_ASYNC);
 	munmap(compiled_code, page_size);
 }
-
+/*
 void* drecompile(uint8_t* func)
 {
 	int i = 0, j = 0;
-
-#ifdef dynamic
-	while (func[i] != 0xc3 && i < page_size && j < page_size) {
+// #ifdef dynamic
+	while (Operation[i] != 0xc3 && Operation[i] != 0xC3) {
 		uint8_t tmp = 0;
-		if (func[i] == 0x83 && func[i + 1] == 0xc0) // add
+		if (Operation[i] == 0x83) // add || sub
 		{
-			for (; func[i] == 0x83 && func[i + 1] == 0xc0; i += 3)
-				tmp += func[i + 2];
-			compiled_code[j++] = func[i - 3];
-			compiled_code[j++] = func[i - 2];
+			for (; Operation[i] == 0x83 && Operation[i + 1] == 0xc0; i += 3)
+				tmp += Operation[i + 2];
+			compiled_code[j++] = Operation[i - 3];
+			compiled_code[j++] = Operation[i - 2];
 			compiled_code[j++] = tmp;
 		}
-		else if (func[i] == 0x83 && func[i + 1] == 0xe8) // sub
-		{
-			for (; func[i] == 0x83 && func[i + 1] == 0xc0; i += 3)
-				tmp -= func[i + 2];
-			compiled_code[j++] = func[i - 3];
-			compiled_code[j++] = func[i - 2];
-			compiled_code[j++] = tmp;
-		}
-		else if (func[i] == 0x6b) // imul
+		else if (Operation[i] == 0x6b) // imul
 		{
 			tmp = 1;
-			for (; func[i] == 0x83 && func[i + 1] == 0xc0; i += 3)
-				tmp *= func[i + 2];
-			compiled_code[j++] = func[i - 3];
-			compiled_code[j++] = func[i - 2];
+			for (; Operation[i] == 0x6b; i += 3)
+				tmp *= Operation[i + 2];
+			compiled_code[j++] = Operation[i - 3];
+			compiled_code[j++] = Operation[i - 2];
 			compiled_code[j++] = tmp;
 		}
-		else if (func[i] == 0xf6) // div
+		else if (Operation[i] == 0xf6) // div
 		{
-			i += 2;
+			j++;
+			i++;
+			// compiled_code[j] = 
+			compiled_code[j++] = Operation[i++];
 		}
 		else // nothing
-			compiled_code[j++] = func[i++];
+		{
+			compiled_code[j++] = Operation[i++];
+		}
 	}
-	printf("DYNAMIC!\n");
-	compiled_code[j - 1] = 0xc3;
-#endif
+	compiled_code[j] = 0xC3;
+// #endif
 	mprotect(compiled_code, page_size, PROT_READ | PROT_EXEC);
 	return compiled_code;
 }
+*/
 
+void* drecompile(uint8_t* func)
+{
+#ifdef dynamic
+	int i = 0, j = 0;
+	uint8_t code[3];
+
+	// for all function instruction
+	while(func[i] != 0xc3) {
+		// operation, source register
+		code[0] = func[i];
+
+		// op is not target of optimization
+		if(code[0] != 0x83 && code[0] != 0x6b && code[0] != 0xf6) {
+			compiled_code[j++] = func[i++];
+		}
+		else {
+			// init value
+			code[1] = func[i+1];
+			code[2] = code[0] == 0x83 ? 0 : 1;
+
+			if(code[0] == 0xf6) {	// div
+				// same code[0-1]
+				while(func[i] == code[0] && func[i+1] == code[1]) {
+					code[2] *= func[i-1];
+
+					// if repeated div, i=i+4
+					// else,	    i=i+2
+					i = func[i+4] == code[0] ? i+4 : i+2;
+				}
+				compiled_code[j-1] = code[2];
+				compiled_code[j++] = code[0];
+				compiled_code[j++] = code[1];
+			}
+			else {			// not div
+				// same code[0-1]
+				while(func[i] == code[0] && func[i+1] == code[1]) {
+					if(code[0] == 0x83) // add suv
+						code[2] += func[i+2];
+					else		    // imul
+						code[2] *= func[i+2];
+					i += 3;
+				}
+				compiled_code[j++] = code[0];
+				compiled_code[j++] = code[1];
+				compiled_code[j++] = code[2];
+			}
+		}
+	}
+	// end of func
+	compiled_code[j] = func[i];
+#endif
+
+	// chmod r-x
+	mprotect(compiled_code, PAGE_SIZE, PROT_READ | PROT_EXEC);
+
+	return compiled_code;
+}
