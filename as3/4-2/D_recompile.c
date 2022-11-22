f0491b92578c8ae540cba75149f0ce5c8dcd74b7
YEONGORI <dusrjf14@gmail.com>
2022-11-22 01:24:57 -0800

    add: OS Assignment 4-2

--- as3/4-2/D_recompile.c
+++ as3/4-2/D_recompile.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <stdint.h> 
+#include <string.h>
 #include <stdlib.h>
 #include <sys/shm.h>
 #include <sys/stat.h>
 #include <sys/user.h>
 #include <sys/mman.h>
+#include <sys/types.h>
+
+int	segment_id, page_size;
 
 uint8_t* Operation;
 uint8_t* compiled_code;
 
-void sharedmem_init(); // °øÀ¯ ¸Þ¸ð¸®¿¡ Á¢±Ù
+void sharedmem_init(); // ï¿½ï¿½ï¿½ï¿½ ï¿½Þ¸ð¸®¿ï¿½ ï¿½ï¿½ï¿½ï¿½
 void sharedmem_exit();
-void drecompile_init(); // memory mapping ½ÃÀÛ 
+void drecompile_init(); // memory mapping ï¿½ï¿½ï¿½ï¿½ 
 void drecompile_exit(); 
-void* drecompile(uint8_t *func); //ÃÖÀûÈ­ÇÏ´Â ºÎºÐ
+void* drecompile(uint8_t *func); //ï¿½ï¿½ï¿½ï¿½È­ï¿½Ï´ï¿½ ï¿½Îºï¿½
 
 int main(void)
 {
@@ -32,21 +37,93 @@ int main(void)
 
 void sharedmem_init()
 {
+	segment_id = shmget(1234, PAGE_SIZE, IPC_CREAT | S_IRUSR | S_IWUSR);
+	Operation = (uint8_t*)shmat(segment_id, NULL, 0);
+
 }
 
 void sharedmem_exit()
 {
+	shmdt(Operation);
+	shmctl(segment_id, IPC_RMID, NULL);
 }
 
-void drecompile_init(uint8_t *func)
+void drecompile_init()
 {
+	page_size = getpagesize();
+
+	compiled_code = mmap(NULL, page_size, PROT_READ | PROT_WRITE, MAP_SHARED, -1, 0);
+	memcpy(compiled_code, Operation, page_size);
 }
 
 void drecompile_exit()
 {
+	munmap(compiled_code, page_size);
 }
 
 void* drecompile(uint8_t* func)
 {
+	int i = 0, j = 0;
+	uint8_t tmp;
+
+#ifdef DYNAMIC
+	while (func[i] != 0xc3) {
+		tmp = 0;
+		if (func[i] == 0x83 && func[i + 1] == 0xc0) // add
+		{
+			for (; func[i] == 0x83 && func[i + 1] == 0xc0; i += 3)
+				tmp += func[i + 2];
+			compiled_code[j++] = func[i - 3];
+			compiled_code[j++] = func[i - 2];
+			compiled_code[j++] = tmp;
+			// while (func[i] == 0x83 && func[i + 1] == 0xc0)
+			// {
+			// 	tmp += func[i + 2];
+			// 	i += 3;
+			// }
+
+		}
+		else if (func[i] == 0x83 && func[i + 1] == 0xe8) // sub
+		{
+			for (; func[i] == 0x83 && func[i + 1] == 0xc0; i += 3)
+				tmp -= func[i + 2];
+			compiled_code[j++] = func[i - 3];
+			compiled_code[j++] = func[i - 2];
+			compiled_code[j++] = tmp;
+			// while (func[i] == 0x83 && func[i + 1] == 0xc0)
+			// {
+			// 	tmp -= func[i + 2];
+			// 	i += 3;
+			// }
+		}
+		else if (func[i] == 0x6b) // imul
+		{
+			tmp = 1;
+			for (; func[i] == 0x83 && func[i + 1] == 0xc0; i += 3)
+				tmp *= func[i + 2];
+			compiled_code[j++] = func[i - 3];
+			compiled_code[j++] = func[i - 2];
+			compiled_code[j++] = tmp;
+			// while (func[i] == 0x6b)
+			// {
+			// 	tmp
+			// }
+		}
+		else if (func[i] == 0xf6) // div
+		{
+			i += 2;
+			// while (func[i] == 0xf6)
+			// {
+			// 	tmp *= func[i + 2];
+			// 	i += 3;
+			// }
+		}
+		else
+			compiled_code[j++] = func[i++];
+	}
+	compiled_code[j] = func[i];
+#endif
+	mprotect(compiled_code, page_size, PROT_READ | PROT_EXEC);
+	return compiled_code;
 }
 
